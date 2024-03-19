2a9aaeab2d2bfb95095a8326a96e3851302e58df
Kamo develop <kamodeveloper@gmail.com>
2024-03-19 20:16:58 +0700

    trying to use proxy

--- phones_crawler/settings.py
+++ phones_crawler/settings.py
@@ -6,8 +6,16 @@ NEWSPIDER_MODULE = "phones_crawler.spiders"
 ROBOTSTXT_OBEY = False
 
 DOWNLOADER_MIDDLEWARES = {
+    # "rotating_proxies.middlewares.RotatingProxyMiddleware": 510,
+    # "rotating_proxies.middlewares.BanDetectionMiddleware": 520,
     "phones_crawler.middlewares.PhonesCrawlerDownloaderMiddleware": 543,
 }
+
+# ROTATING_PROXY_LIST = [
+#     '14.99.212.242:5678',
+#     '188.132.146.23:1080',
+#     '43.153.64.66:443',
+# ]
 
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
